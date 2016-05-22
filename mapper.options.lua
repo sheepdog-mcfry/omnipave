f69e616ca2861fbc915db112226d48d3d869f899
sheepdog-mcfry <sheepdog.mcfry@gmail.com>
2016-05-22 08:26:07 +0200

    Initial Commit

---
+++ mapper.options.lua
@@ -0,0 +1,4 @@
+return {{[ [[options]] ] = {2},[ [[locked_areas]] ] = {3},},
+{[ [[sprint]] ] = false,[ [[lockwormholes]] ] = true,[ [[duanatharan]] ] = false,[ [[caravan]] ] = false,[ [[lockpathways]] ] = true,[ [[slowwalk]] ] = false,[ [[locksewers]] ] = false,[ [[lockspecials]] ] = false,[ [[shackle]] ] = false,[ [[gallop]] ] = false,[ [[runaway]] ] = false,[ [[showcmds]] ] = true,[ [[dash]] ] = false,[ [[crowdmap]] ] = false,[ [[removewings]] ] = true,[ [[duanathar]] ] = false,[ [[updatemap]] ] = true,[ [[winglanguage]] ] = [[]],},
+{},
+}
